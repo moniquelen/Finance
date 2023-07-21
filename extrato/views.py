28d5370082cae9caec8036784496ccf13866babd
Monique Elen <moniquelen10@hotmail.com>
2023-07-21 12:34:24 -0300

    view extrato

--- extrato/views.py
+++ extrato/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from .models import Valores
 from django.contrib import messages
 from django.contrib.messages import constants
+from datetime import datetime
 
 def novo_valor(request):
     if request.method == 'GET':
@@ -41,3 +42,20 @@ def novo_valor(request):
         conta.save()
         
         return redirect('/extrato/novo_valor/')
+    
+def view_extrato(request):
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    
+    conta_get = request.GET.get('conta')
+    categoria_get = request.GET.get('categoria')
+    
+    valores = Valores.objects.filter(data__month=datetime.now().month) # para trazer só os valores do mês atual
+    
+    if conta_get:
+        valores = valores.filter(conta__id=conta_get)
+        
+    if categoria_get:
+        valores = valores.filter(categoria__id=categoria_get)
+        
+    return render(request, 'view_extrato.html', {'valores' : valores, 'contas' : contas, 'categorias' : categorias})

33fc1e506d6095f6fe5b551272ecb252d4961c1c
Monique Elen <moniquelen10@hotmail.com>
2023-09-02 23:12:36 -0300

    update planejamento

--- planejamento/urls.py
+++ planejamento/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('definir_planejamento/', views.definir_planejamento, name="definir_planejamento"),
+    path('update_valor_categoria/<int:id>', views.update_valor_categoria, name="update_valor_categoria"),
+    
 ]
--- planejamento/views.py
+++ planejamento/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from perfil.models import Categoria
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def definir_planejamento(request):
     categorias = Categoria.objects.all()
     return render(request, 'definir_planejamento.html', {'categorias': categorias})
+
+@csrf_exempt #isenta de validação csrf token
+def update_valor_categoria(request, id):
+    novo_valor = json.load(request)['novo_valor']
+    categoria = Categoria.objects.get(id=id)
+    categoria.valor_planejamento = novo_valor
+    categoria.save()
+
+    return JsonResponse({'status': 'Sucesso'})
